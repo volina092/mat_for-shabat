ecb3ee14d2fe2507933aba2a954c85dba1a44d21
volina092 <45100374+volina092@users.noreply.github.com>
2020-12-26 15:53:13 +0300

    Update table_s3_mat.py

--- table_s3_mat.py
+++ table_s3_mat.py
@@ -1,11 +1,10 @@
-def operation(line_01, line_02): # однократна€ операци€ дл€ двух конкретных эл-тов
+def operation(line_01, line_02):
     act = ''
     for i in range(3):
         act += s3[line_01][int(s3[line_02][i]) - 1]
-    #print(act) где act Ч результат операции дл€ двух конкретных эл-тов
     return ctrl_s3[act]
 
-def table(): # "печать" таблицы с результатами операций дл€ всех пар эл-тов
+def table(): 
     x_row, y_row = sorted(s3.keys()), sorted(s3.keys())
     print('', *x_row, sep='\t')
     for y in y_row:
@@ -20,8 +19,8 @@ s3 = {'1': '123',
       'd': '213',
       'a': '231',
       'b': '312',
-      'e': '321'} # "алфавит"
-ctrl_s3 = {} # "обратный алфавит": от значени€ к символу
+      'e': '321'} 
+ctrl_s3 = {}
 for i in s3.keys(): ctrl_s3[s3[i]] = i 
 
 table()
